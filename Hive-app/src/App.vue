75c8a988d7778f5a07d4c7413b2cad0e5b1d0e69
Nam3less-0 <163498726+Nam3less-0@users.noreply.github.com>
2025-02-21 17:18:05 +0800

    Login Page

--- Hive-app/src/App.vue
+++ Hive-app/src/App.vue
@@ -1,47 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <Login />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import Login from "@/components/Login.vue";
+</script>
+
+<style>
+/* ✅ ENSURE FULL WIDTH */
+#app {
+  width: 100vw !important;
+  height: 100vh !important;
+  display: flex;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  max-width: max-content;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* ✅ PREVENT ANY CONTAINER FROM SHRINKING */
+body, html {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  display: flex;
 }
 </style>
